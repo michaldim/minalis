f5470519cb18e65f308a2e7ba8d15ca2fa1ebf1a
Michal Diminsky <michal.diminsky@gmail.com>
2021-02-16 22:13:32 +0200

    changed the location of the button script out of the html file and into the script file

--- scripts/Savta-Home.js
+++ scripts/Savta-Home.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+
+	//Get the button
+	var mybutton = document.getElementById("myBtn");
+
+	// When the user scrolls down 600px from the top of the document, show the button
+	window.onscroll = scrollFunction;
+
+	function scrollFunction() {
+		 if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+		    mybutton.style.display = "block";
+		 } else {
+		    mybutton.style.display = "none";
+		 }
+	}
+
+	// When the user clicks the button, the page will be scrolled up to the top of the document
+	mybutton.onclick = topFunction;
+
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+		
 	//$('[data-fancybox="gallery"]').fancybox({
 	// Options will go here
 	//});
