dfd035ed6a4d57d63013a9509161f17cdef09be8
Michal Diminsky <michal.diminsky@gmail.com>
2021-03-07 15:38:48 +0200

    mobile orientation + smooth movement of header in mobile

--- scripts/Savta-Home.js
+++ scripts/Savta-Home.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
 
+	//There is more than one "scroll" event on window, so we have to use addEventListener, because it's preventing same events to override one another
+	window.addEventListener("scroll", headerDisappearsFunction);
+	window.addEventListener("scroll", buttonScrollFunction);
+
+	/* When the user scrolls down, hide the header. When the user scrolls to the top of the page, show the header */
+	var startingScrollPos = 0; 
+	function headerDisappearsFunction() {
+	  var currentScrollPos = window.pageYOffset; //pageYOffset tells us how much the screen was scrolled on the Y axis (0 means it wasn't scrolled)
+	  if (screen.width < 601 && (startingScrollPos < currentScrollPos)) {
+	    document.getElementsByTagName("header")[0].style.top = "-60px";
+	  } else {
+	    document.getElementsByTagName("header")[0].style.top = "0";
+	  }
+	}
+
 
 	//Get the button
 	var mybutton = document.getElementById("myBtn");
 
-	// When the user scrolls down 600px from the top of the document, show the button
-	window.onscroll = scrollFunction;
-
-	function scrollFunction() {
-		 if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+	//The screen needs to be bigger than 600px and when someone scrolls down more than 600px from the top of the document, than the button will appear
+	function buttonScrollFunction() {
+		 if (screen.width > 600 && (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600)) {   
 		    mybutton.style.display = "block";
 		 } else {
 		    mybutton.style.display = "none";
@@ -16,6 +29,24 @@ $(document).ready(function(){
 	}
 
 	
+
+
+	//make the mobile screen portrait at all times
+	window.addEventListener("load", makeTheScreenPortrait);
+	window.addEventListener("orientationchange", makeTheScreenPortrait);
+
+	var body = document.getElementsByTagName("body")[0];
+
+	function makeTheScreenPortrait() {
+		if (screen.width < 825 && (window.innerHeight < window.innerWidth)){
+			body.style.height = "100vw";
+			body.style.transform = "rotate(-90deg)";
+			body.style.msTransform = "rotate(-90deg)";
+		}
+	}
+
+
+
 	//The next code uses jQuery plug-in:"scripts/smooth-scroll.polyfills.js". 
 	//It makes the in-page anchors to have a smoothScroll in the speed that I choose. 
 	//Adittional info about this code in https://github.com/cferdinandi/smooth-scroll
@@ -42,6 +73,19 @@ $(document).ready(function(){
 
 	
 
+
+	
+
+
+	///////////////&& (navigator.userAgent.indexOf( "Mobile" ) !== -1) 
+	// body {
+	// 		    height: 100vw;
+	// 		    -webkit-transform: rotate(-90deg);
+	// 		    -moz-transform: rotate(-90deg);
+	// 		    -o-transform: rotate(-90deg);
+	// 		    -ms-transform: rotate(-90deg);
+	// 		    transform: rotate(-90deg);
+	// 		  }
 
 
 });
